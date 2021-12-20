6c63796a550ff270f9fea13ad5b03596f018f4f6
Grégory Bataille <gregory.bataille@gmail.com>
2021-12-20 07:39:35 +0100

    AoC day1

--- 1/main.py
+++ 1/main.py
@@ -1,2 +1,16 @@
+from pathlib import Path
+
+
 def main():
-    print("1")
+    input_path = Path(__file__).parent.joinpath('input1.txt')
+    input_data = input_path.read_text()
+
+    cnt = 0
+    lines = list(map(int, input_data.strip().split('\n')))
+    for idx in range(len(lines) - 3):
+        after = lines[idx + 1] + lines[idx + 2] + lines[idx + 3]
+        before = lines[idx] + lines[idx + 1] + lines[idx + 2]
+        if after > before:
+            cnt += 1
+
+    print(cnt)
