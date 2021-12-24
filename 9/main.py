697374b68cc3d189cf9f66079782f676e27f6d11
Grégory Bataille <gregory.bataille@gmail.com>
2021-12-24 11:51:11 +0100

    day9

--- 9/main.py
+++ 9/main.py
@@ -1,11 +1,14 @@
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional, Set, Tuple
 
-from utils import aoc_input
+from utils import aoc_input, geo
 from utils.out import debug
 
-
-TEST_DATA = ""
+TEST_DATA = """2199943210
+3987894921
+9856789892
+8767896789
+9899965678"""
 
 
 def main_part_1(input_file: Optional[Path]) -> None:
@@ -13,11 +16,55 @@ def main_part_1(input_file: Optional[Path]) -> None:
     if input_file is not None:
         input_data = input_file.read_text()
 
+    heightmap = geo.Array2D[int].from_string(input_data, int)
+    low_points: List[geo.Point2D] = []
+
+    out = 0
+
+    for x, y, v in heightmap.step_through():
+        neighbours = list(map(lambda j: j[2], heightmap.neighbours(x, y)))
+        if v < min(neighbours):
+            low_points.append(geo.Point2D(x, y))
+            out += 1 + v
+
+    print(out)
+
 
 def main_part_2(input_file: Optional[Path] = None) -> None:
     input_data = TEST_DATA
     if input_file:
         input_data = input_file.read_text()
+
+    heightmap = geo.Array2D[int].from_string(input_data, int)
+    low_points: List[geo.Point2D] = []
+
+    for x, y, v in heightmap.step_through():
+        neighbours = list(map(lambda j: j[2], heightmap.neighbours(x, y)))
+        if v < min(neighbours):
+            low_points.append(geo.Point2D(x, y))
+
+    def extend_basin(basin: Set[geo.Point2D]):
+        len_in = len(basin)
+        addtl_points: List[Tuple[int, int, int]] = []
+        for point in basin:
+            addtl_points += list(heightmap.neighbours(point.x, point.y))
+        basin = basin | set(map(lambda j: geo.Point2D(j[0], j[1]), filter(lambda x: x[2] != 9, addtl_points)))
+        len_out = len(basin)
+        if len_in == len_out:
+            return basin
+
+        return extend_basin(basin)
+
+    basins: List[Set[int]] = []
+    basins_sizes = []
+    for point in low_points:
+        basin = [point]
+        basin = extend_basin(set(basin))
+        basins.append(basin)
+        basins_sizes.append(len(basin))
+
+    basins_sizes.sort()
+    print(basins_sizes[-1] * basins_sizes[-2] * basins_sizes[-3])
 
 
 def main(day_num: int, part_num: int, with_test_data=True) -> None:
--- utils/geo.py
+++ utils/geo.py
@@ -1,5 +1,6 @@
+from copy import deepcopy
 from dataclasses import dataclass
-from typing import Generic, List, TypeVar
+from typing import Callable, Generator, Generic, List, Optional, Tuple, TypeVar
 
 
 @dataclass
@@ -7,12 +8,13 @@ class Point2D:
     x: int
     y: int
 
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Point2D):
+            return False
+        return self.x == other.x and self.y == other.y
 
-@dataclass
-class Point3D:
-    x: int
-    y: int
-    z: int
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
 
 
 T = TypeVar('T')
@@ -21,10 +23,113 @@ T = TypeVar('T')
 @dataclass
 class Array2D(Generic[T]):
     cells: List[List[T]]
+    height: int
+    width: int
+
+    def __init__(
+        self,
+        cells: List[List[T]],
+    ):
+        self.cells = cells
+        self.width = len(cells[0])
+        self.height = len(cells)
 
     def get_at(self, x: int, y: int) -> T:
         return self.cells[y][x]
 
     def insert_at(self, x: int, y: int, value: T) -> 'Array2D':
+        if x > self.width or x < 0:
+            raise IndexError(f'x {x} is outside the array boundaries')
+        if y > self.height or y < 0:
+            raise IndexError(f'y {y} is outside the array boundaries')
+
         self.cells[y][x] = value
         return self
+
+    def insert_at_point(self, p: Point2D, value: T) -> 'Array2D':
+        return self.insert_at(p.x, p.y, value)
+
+    def step_through(self, horizontally: bool = True) -> Generator[Tuple[int, int, T], None, None]:
+        if horizontally:
+            for y in range(self.height):
+                for x in range(self.width):
+                    yield (x, y, self.cells[y][x])
+        else:
+            for x in range(self.width):
+                for y in range(self.height):
+                    yield (x, y, self.get_at(x, y))
+
+    def neighbours(self, x: int, y: int, include_diags: bool = False) -> Generator[Tuple[int, int, T], None, None]:
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                # skip diag
+                if not include_diags and dx != 0 and dy != 0:
+                    continue
+
+                # skip self
+                if dx == 0 and dy == 0:
+                    continue
+
+                # Handle borders
+                if not 0 <= x + dx < self.width:
+                    continue
+                if not 0 <= y + dy < self.height:
+                    continue
+
+                yield (x + dx, y + dy, self.get_at(x + dx, y + dy))
+
+    @staticmethod
+    def from_string(
+        input_str: str,
+        str_elem_to_T_elem: Callable[[str], T],
+        line_separator: str = '\n',
+        elem_separator: Optional[str] = None,
+    ) -> 'Array2D[T]':
+        cells: List[List[T]] = []
+        for line_str in input_str.strip().split(line_separator):
+            line = []
+
+            line_list = []
+            if elem_separator:
+                line_list = line_str.strip().split(elem_separator)
+            else:
+                line_list = list(line_str.strip())
+
+            for elem in line_list:
+                line.append(str_elem_to_T_elem(elem))
+
+            cells.append(line)
+
+        return Array2D(cells)
+
+    def to_string(
+        self,
+        elem_to_str: Callable[[T], str] = str,
+        line_separator: str = '\n',
+        elem_separator: str = '',
+    ):
+        out = ''
+        for line in self.cells:
+            line_str = ''
+            for elem in line:
+                line_str += elem_separator + elem_to_str(elem)
+
+            if elem_separator != '':
+                line_str = line_str[1:]
+
+            out += line_str + line_separator
+
+        return out
+
+    @staticmethod
+    def empty(width: int, height: int, elem: T) -> 'Array2D':
+        cells = []
+
+        line = []
+        for _ in range(width):
+            line.append(elem)
+
+        for _ in range(height):
+            cells.append(deepcopy(line))
+
+        return Array2D(cells)
