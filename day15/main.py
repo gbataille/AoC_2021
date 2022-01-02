22dd4d70b64040a347e28917cab3ccf1ef1071a5
Grégory Bataille <gregory.bataille@gmail.com>
2022-01-02 09:41:03 +0100

    day16

--- day15/main.py
+++ day15/main.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import List, Optional, Set
 
-from utils import aoc_input, geo
+from utils import aoc_input, geo, strings
 from utils.out import debug
 
 TEST_DATA = """1163751742
@@ -19,10 +19,8 @@ TEST_DATA = """1163751742
 
 @dataclass
 class SubmarinePath:
-    cells: List[geo.Cell[geo.Point2D, int]] = field(default_factory=list)
-    seen: Set[geo.Cell[geo.Point2D, int]] = field(default_factory=set)
+    head: geo.Cell[geo.Point2D, int]
     weight: int = 0
-    is_done: bool = False
 
 
 @dataclass
@@ -30,35 +28,40 @@ class Solver:
     chitons: geo.Array2D[int]
     min_map: geo.Array2D[int]
     paths: List[SubmarinePath]
-    done_path: Optional[SubmarinePath] = None
+    best: int = -1
 
     def move1(self) -> 'Solver':
         new_paths: List['SubmarinePath'] = []
+        last_cell = self.min_map.get_cell_at(self.min_map.width - 1, self.min_map.height - 1)
         for path in self.paths:
-            last_cell = path.cells[-1]
-            for cell in self.chitons.neighbours(last_cell.coordinates.x, last_cell.coordinates.y):
-                is_last_cell = (cell.coordinates.x == self.chitons.width - 1
-                                and cell.coordinates.y == self.chitons.height - 1)
+            for cell in self.chitons.neighbours(path.head.coordinates.x, path.head.coordinates.y):
                 new_weight = path.weight + cell.value
-                new_path = SubmarinePath(
-                    cells=list(path.cells) + [cell],
-                    seen=path.seen | set([cell]),
-                    weight=new_weight,
-                    is_done=is_last_cell,
-                )
-                if cell not in path.seen:
-                    if is_last_cell:
-                        if self.done_path is None or new_weight < self.done_path.weight:
-                            self.done_path = new_path
-                    else:
-                        min_for_cell = self.min_map.get_cell_at(cell.coordinates.x, cell.coordinates.y)
-                        if min_for_cell.value == -1 or min_for_cell.value > new_weight:
-                            min_for_cell.value = new_weight
-                            new_paths.append(new_path)
+                min_for_cell = self.min_map.get_cell_at(cell.coordinates.x, cell.coordinates.y)
+
+                if ((min_for_cell.value == -1 or min_for_cell.value > new_weight)
+                        and (last_cell.value == -1 or new_weight < last_cell.value)):
+                    min_for_cell.value = new_weight
+                    new_paths.append(SubmarinePath(head=cell, weight=new_weight))
 
         self.paths = new_paths
 
         return self
+
+    def solve(self):
+        self.extend(self.chitons.get_cell_at(0, 0))
+
+    def extend(self, cell: geo.Cell[geo.Point2D, int]):
+        min_map_cell = self.min_map.get_cell_at(cell.coordinates.x, cell.coordinates.y)
+        current_weight = min_map_cell.value
+        for neighbour in self.chitons.neighbours(cell.coordinates.x, cell.coordinates.y):
+            min_map_neighbour = self.chitons.get_cell_at(neighbour.coordinates.x, neighbour.coordinates.y)
+            neighbour_weight = min_map_neighbour.value
+
+            if neighbour_weight == -1 or neighbour_weight > current_weight + cell.value:
+                min_map_neighbour.value = current_weight + cell.value
+                if not (neighbour.coordinates.x == self.chitons.width - 1
+                        and neighbour.coordinates.y == self.chitons.height - 1):
+                    self.extend(neighbour)
 
 
 def main_part_1(input_file: Optional[Path]) -> None:
@@ -69,19 +72,78 @@ def main_part_1(input_file: Optional[Path]) -> None:
     chitons: geo.Array2D[int] = geo.Array2D.from_string(input_data, int)
     min_map: geo.Array2D[int] = geo.Array2D.empty(chitons.width, chitons.height, -1)
     start = chitons.get_cell_at(0, 0)
-    paths: List[SubmarinePath] = [SubmarinePath(cells=[start], seen=set([start]))]
+    paths: List[SubmarinePath] = [SubmarinePath(head=start)]
 
     solver = Solver(chitons, min_map, paths)
     while solver.paths:
         solver.move1()
+        print(
+            solver.min_map.to_string(
+                lambda x: '  .' if x == -1 else str(x).zfill(3).replace('0', ' '),
+                elem_separator=' ',
+            ))
+        input("press enter")
+        print('')
 
-    print(solver.done_path.weight)
+    print(solver.min_map.get_cell_at(solver.min_map.width - 1, solver.min_map.height - 1))
 
 
 def main_part_2(input_file: Optional[Path] = None) -> None:
     input_data = TEST_DATA
     if input_file:
         input_data = input_file.read_text()
+
+    tile: geo.Array2D[int] = geo.Array2D.from_string(input_data, int)
+    chitons: geo.Array2D[int] = geo.Array2D.empty(tile.width * 5, tile.height * 5, -1)
+    for chiton_cell in chitons.step_through():
+        tile_num_x = int(chiton_cell.coordinates.x / tile.width)
+        projected_tile_x = chiton_cell.coordinates.x % tile.width
+        tile_num_y = int(chiton_cell.coordinates.y / tile.height)
+        projected_tile_y = chiton_cell.coordinates.y % tile.height
+        chiton_value = tile.get_cell_at(projected_tile_x, projected_tile_y).value + tile_num_x + tile_num_y
+        if chiton_value > 9:
+            chiton_value -= 9
+        chiton_cell.value = chiton_value
+
+    min_map: geo.Array2D[int] = geo.Array2D.empty(chitons.width, chitons.height, -1)
+    min_map.set_value_at(0, 0, 0)
+    start = chitons.get_cell_at(0, 0)
+    # paths: List[SubmarinePath] = [SubmarinePath(head=start)]
+    heads: List[geo.Cell[geo.Point2D, int]] = [start]
+
+    print('algo')
+    while heads:
+        head = heads.pop()
+        for neighbour in chitons.neighbours(head.coordinates.x, head.coordinates.y):
+            neighbour_weight = min_map.get_value_at(head.coordinates.x, head.coordinates.y) + neighbour.value
+            neighbour_cur_value = min_map.get_value_at(neighbour.coordinates.x, neighbour.coordinates.y)
+            if neighbour_cur_value == -1 or neighbour_weight < neighbour_cur_value:
+                min_map.set_value_at(neighbour.coordinates.x, neighbour.coordinates.y, neighbour_weight)
+                heads.append(neighbour)
+
+        # print(
+        #     min_map.to_string(
+        #         lambda x: '  .' if x == -1 else strings.replace_leading_x('0', ' ',
+        #                                                                   str(x).zfill(3)),
+        #         elem_separator=' ',
+        #     ))
+        # input("press enter")
+        # print('')
+
+    print(min_map.get_cell_at(min_map.width - 1, min_map.height - 1))
+
+    # ATTEMPT 1
+    # solver = Solver(chitons, min_map, paths)
+    # while solver.paths:
+    #     solver.move1()
+    #
+    # print(solver.min_map.get_cell_at(solver.min_map.width - 1, solver.min_map.height - 1))
+
+    # ATTEMPT 2
+    # solver2 = Solver(chitons, min_map, paths)
+    # solver2.extend(solver2.chitons.get_cell_at(0, 0))
+    #
+    # print(solver2.min_map.get_cell_at(solver2.min_map.width - 1, solver2.min_map.height - 1))
 
 
 def main(day_num: int, part_num: int, with_test_data=True) -> None:
--- day16/main.py
+++ day16/main.py
@@ -1,11 +1,118 @@
+from dataclasses import dataclass, field
+from functools import reduce
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional, Tuple
 
 from utils import aoc_input
 from utils.out import debug
 
+TEST_DATA = "CE00C43D881120"
 
-TEST_DATA = ""
+HEX_TO_BIN = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'A': '1010',
+    'B': '1011',
+    'C': '1100',
+    'D': '1101',
+    'E': '1110',
+    'F': '1111',
+}
+
+
+@dataclass
+class Packet:
+    version: int
+    type_id: int
+    literal_value: Optional[int] = None
+    children: List['Packet'] = field(default_factory=list)
+
+    @property
+    def value(self) -> int:
+        if self.type_id == 0:
+            return reduce(lambda x, y: x + y, map(lambda x: x.value, self.children))
+        elif self.type_id == 1:
+            return reduce(lambda x, y: x * y, map(lambda x: x.value, self.children))
+        elif self.type_id == 2:
+            return reduce(min, map(lambda x: x.value, self.children))
+        elif self.type_id == 3:
+            return reduce(max, map(lambda x: x.value, self.children))
+        elif self.type_id == 4:
+            return self.literal_value or 0
+        elif self.type_id == 5:
+            return 1 if self.children[0].value > self.children[1].value else 0
+        elif self.type_id == 6:
+            return 1 if self.children[0].value < self.children[1].value else 0
+        elif self.type_id == 7:
+            return 1 if self.children[0].value == self.children[1].value else 0
+        else:
+            raise ValueError(f'Unknown type {self.type_id}')
+
+
+def parse_literal(input_string: str) -> Tuple[int, int]:
+    cursor = 0
+    done = False
+    binary_str = ''
+    while not done:
+        done = not bool(int(input_string[cursor]))
+        binary_str += input_string[cursor + 1:cursor + 5]
+        cursor += 5
+
+    return (int(binary_str, 2), cursor)
+
+
+def parse_sub_packets(input_string: str, max_packets: int = 0) -> Tuple[List[Packet], int]:
+    cursor = 0
+    packets: List[Packet] = []
+    try:
+        while max_packets == 0 or len(packets) < max_packets:
+            if all(map(lambda x: x == '0', input_string[cursor:])):
+                print('clean exit')
+                break
+
+            version = int(input_string[cursor:cursor + 3], 2)
+            type_id = int(input_string[cursor + 3:cursor + 6], 2)
+
+            if type_id == 4:
+                value, next_idx = parse_literal(input_string[cursor + 6:])
+                packets.append(Packet(version, type_id, literal_value=value))
+                cursor += 6 + next_idx
+
+            else:
+                length_type_id = int(input_string[cursor + 6])
+                if length_type_id == 0:
+                    length = int(input_string[cursor + 7:cursor + 22], 2)
+                    sub_packets_str = input_string[cursor + 22:cursor + 22 + length]
+                    sub_packets, _ = parse_sub_packets(sub_packets_str)
+                    packets.append(Packet(version, type_id, children=sub_packets))
+                    cursor = cursor + 22 + length
+                else:
+                    nb_packets = int(input_string[cursor + 7:cursor + 18], 2)
+                    sub_packets, next_idx = parse_sub_packets(input_string[cursor + 18:], max_packets=nb_packets)
+                    packets.append(Packet(version, type_id, children=sub_packets))
+                    cursor += 18 + next_idx
+
+    except (IndexError, ValueError):
+        print('ugly exit')
+
+    return packets, cursor
+
+
+def total_version(packets: List[Packet]) -> int:
+    total = 0
+    for packet in packets:
+        total += total_version(packet.children)
+        total += packet.version
+
+    return total
 
 
 def main_part_1(input_file: Optional[Path]) -> None:
@@ -13,11 +120,27 @@ def main_part_1(input_file: Optional[Path]) -> None:
     if input_file is not None:
         input_data = input_file.read_text()
 
+    bits = ''
+    for hex_char in input_data.strip():
+        bits += HEX_TO_BIN[hex_char]
+
+    packets, _ = parse_sub_packets(bits)
+
+    print(total_version(packets))
+
 
 def main_part_2(input_file: Optional[Path] = None) -> None:
     input_data = TEST_DATA
     if input_file:
         input_data = input_file.read_text()
+
+    bits = ''
+    for hex_char in input_data.strip():
+        bits += HEX_TO_BIN[hex_char]
+
+    packets, _ = parse_sub_packets(bits)
+
+    print(packets[0].value)
 
 
 def main(day_num: int, part_num: int, with_test_data=True) -> None:
